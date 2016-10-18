4c74f6428fab859e9fa24e58cb3f6de2a583919b
Natalia Carrillo Blanco <ncarrill@alpha15.aulas.gsyc.urjc.es>
2016-10-18 12:49:14 +0200

    A medias ejercicio 6

--- client.py
+++ client.py
@@ -7,19 +7,17 @@ Programa cliente UDP que abre un socket a un servidor
 import sys
 import socket
 
-# Constantes. Dirección IP del servidor y contenido a enviar
-#server = sys.argv[1]
-#port = sys.argv[2]
-SERVER = sys.argv[1]
-port = int(sys.argv[2])
-line = ' '.join(sys.argv[3:])
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+IP = sys.argv[1]
+PORT = int(sys.argv[2])
+METODO = "REGISTER"
+DIRCC = sys.argv[4]
+PETICION_REG = METODO + " sip:" + DIRCC + " SIP/2.0 \r\n\r\n"
+
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, port))
-    print("Enviando:", line)
-    my_socket.send(bytes(line, 'utf-8') + b'\r\n')
+    my_socket.connect((IP, PORT))
+    print("Enviando:", PETICION_REG)
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
-
-print("Socket terminado.")
+    
+print("Socket terminado")

8228e07aa3d85be175a103ecc450772a39b768c1
Natalia Carrillo Blanco <ncarrill@alpha25.aulas.gsyc.urjc.es>
2016-10-20 12:11:41 +0200

    Acabado ejercicio 6 practica 4

--- client.py
+++ client.py
@@ -10,13 +10,16 @@ import socket
 
 IP = sys.argv[1]
 PORT = int(sys.argv[2])
-METODO = "REGISTER"
+METODO = sys.argv[3]
 DIRCC = sys.argv[4]
-PETICION_REG = METODO + " sip:" + DIRCC + " SIP/2.0 \r\n\r\n"
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((IP, PORT))
+    if METODO == "register":
+        PETICION_REG = "REGISTER sip:" + DIRCC + " SIP/2.0 \r\n\r\n"
     print("Enviando:", PETICION_REG)
+    my_socket.send(bytes(PETICION_REG, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
     

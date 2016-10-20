0bc69285add19b1d67c66ffd2ac9a0db1c225f2e
Natalia Carrillo Blanco <ncarrill@alpha25.aulas.gsyc.urjc.es>
2016-10-20 12:11:56 +0200

    Acabado ejercicio 6 practica 4

--- server.py
+++ server.py
@@ -13,20 +13,26 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     SIP Register Handler
     """
 
+    Dicc = {}
+
     def handle(self):
         IP_client = self.client_address[0]
-        Port_client = self.client_address[1]
-        self.wfile.write(b"Hemos recibido tu peticion")
-        for line in self.rfile:
-            print("IP cliente:", IP_client, "Puerto del cliente:", Port_client )
-            print("El cliente nos manda ", line.decode('utf-8'))
+        Port_client = str(self.client_address[1])
+        print('IP cliente:' + IP_client)
+        print('Puerto del cliente:' + Port_client + '\n')
+        peticion_SIP = self.rfile.read().decode('utf-8').split()
+        print(peticion_SIP)
+        if peticion_SIP[0] == 'REGISTER':
+            address = peticion_SIP[1]
+            self.Dicc[address] = self.client_address[0]
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            #if peticion_SIP[-1] == 0:
+                #del self.Dicc[peticion_SIP[1]]
+        print(self.Dicc)
 
 if __name__ == "__main__":
 
     Port_serv = int(sys.argv[1])
     serv = socketserver.UDPServer(('', Port_serv), SIPRegisterHandler)
-    print("Lanzando servidor UDP de eco...")
-    try:
-        serv.serve_forever()
-    except KeyboardInterrupt:
-        print("Finalizado servidor")
+    print('Lanzando servidor UDP de eco...' + '\n')
+    serv.serve_forever()

f8f00b56779b20e1fa30e26741fbd5185567e2a8
nataliacb24 <natalia-cb@live.com>
2016-10-24 23:55:58 +0200

    Ejercicio 8 acabado

--- server.py
+++ server.py
@@ -16,6 +16,32 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
 
     Dicc = {}
+    def register2json(self):
+        """
+        Creacion fichero json con los datos de un diccionario
+        """
+        json.dump(self.Dicc, open('registered.json', 'w'))
+
+    def json2register(self):
+        """
+        Comprobacion del fichero json
+        """
+        try:
+            with open("register.json", 'r') as fichjson:
+                self.Dicc = json.load(fichjson)
+        except:
+            pass
+
+    def delete(self):
+        lista = []
+        for clave in self.Dicc:
+            time_now = self.Dicc[clave][1]
+            print(time_now)
+            time_strp = time.strptime(time_now, '%Y-%m-%d %H:%M:%S')
+            if time_strp <= time.gmtime(time.time()):
+                lista.append(clave)
+        for cliente in lista:
+            del self.Dicc[cliente]
 
     def handle(self):
         """
@@ -25,29 +51,33 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         Port_client = str(self.client_address[1])
         print('IP cliente:' + IP_client)
         print('Puerto del cliente:' + Port_client + '\n')
-        peticion_SIP = self.rfile.read().decode('utf-8').split()
-        if peticion_SIP[0] == 'REGISTER':
-            address = peticion_SIP[1]
-            self.Dicc[address] = self.client_address[0]
+        if len(self.Dicc) == 0:
+            self.json2register()
             self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-            if peticion_SIP[-1] == '0':
-                self.Dicc.clear()
-            else:
-                expire = peticion_SIP[-1]
-                self.Dicc['Expire'] = expire
-        print(self.Dicc)
-        fichjson = self.register2json()
+        while 1:
 
-    def register2json(self):
-        """
-        Creacion fichero json
-        """
-        json.dump(self.Dicc, open('registered.json', 'w'))
+            peticion_SIP = self.rfile.read().decode('utf-8')
+            print(peticion_SIP)
+            if not peticion_SIP:
+                break
+            (metodo, address, sip, space, expire) = peticion_SIP.split()
+            if metodo != 'REGISTER' and not "@" in address:
+                break
+            time_now = int(time.time()) + int(expire)
+            time_exp = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time_now))
+            if expire == '0':
+                del self.Dicc[address]
+            else:
+                self.Dicc[address] = [str(IP_client), str(time_exp)]
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            self.delete()
+            self.register2json()
 
 
 if __name__ == "__main__":
-
     Port_serv = int(sys.argv[1])
     serv = socketserver.UDPServer(('', Port_serv), SIPRegisterHandler)
     print('Lanzando servidor UDP de eco...' + '\n')
+    serv.allow_reuse_address = True
     serv.serve_forever()
+    serv.close()
